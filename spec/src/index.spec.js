a6171f645eeb541f0781e0ed1317885164e559ae
vanessa holland <vanessaholland@legalshieldcorp.com>
2017-05-16 13:46:09 -0500

    testing wip

---
+++ spec/src/index.spec.js
@@ -0,0 +1,19 @@
+var index = require('../../src/index'),
+    framework = require('../alexa-test-framework'),
+    intent = framework.intent,
+    session = framework.session,
+    response = framework.response;
+
+function getEvent(fileName) {
+  let event = require(`../json/requests/${fileName}`);
+  return event;
+}
+
+describe('index', function() {
+var event = getEvent('launchRequestTest.json');
+    it('LaunchRequest', function() {
+        var launch = index.handler(event);
+        console.log(launch);
+        expect(launch).toEqual('Welcome to Ask by Legal Shield. To hear the top featured questions say featured questions, or for free access to more than 1,400 legal questions visit ask dot legal shield dot com or download our mobile app. ')
+    });
+});
