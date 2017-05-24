ebb8dec68747b2b74d9d4ef75ab4cebc810e7103
vanessa holland <vanessaholland@legalshieldcorp.com>
2017-05-23 21:51:06 -0500

    tests are live

--- spec/alexa-test-framework.js
+++
@@ -1,4 +0,0 @@
-module.exports.intent = require('./json/intent'),
-module.exports.session = require('./json/session'),
-module.exports.response = require('./response');
-module.exports.beforeEachMatchers = require('./validators').beforeEachMatchers;
